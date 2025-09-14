b40362b9a4d3a25b0287db738aa1f5cb0358c24b
mohammad reza <mojaver77@gmail.com>
2025-09-14 20:10:51 +0330

    feat: add awake screen

--- src/navigation/BaseNavigator.js
+++ src/navigation/BaseNavigator.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { AppState } from "react-native";
+import IdleTimerManager from "react-native-idle-timer";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useSelector } from "react-redux";
 import { useNavigation } from "@react-navigation/native";
@@ -12,33 +14,48 @@ import routes from "./routes";
 
 const Stack = createNativeStackNavigator();
 
-
 const BaseNavigator = () => {
-    const authenticated = useSelector(selectAuthenticated);
-    const navigation = useNavigation();
+  const authenticated = useSelector(selectAuthenticated);
+  const navigation = useNavigation();
 
-    useDidMountEffect(() => {
-        authenticated ? navigation.navigate(routes.DRAWERNAVIGATOR) :
-            navigation.navigate(routes.AUTHNAVIGATOR)
-    }, [authenticated])
+  useDidMountEffect(() => {
+    authenticated
+      ? navigation.navigate(routes.DRAWERNAVIGATOR)
+      : navigation.navigate(routes.AUTHNAVIGATOR);
+  }, [authenticated]);
 
-    return (
-        <Stack.Navigator
-            screenOptions={{ headerShown: false }}
-            initialRouteName={routes.SPLASH}
-        >
-            <Stack.Screen
-                name={routes.SPLASH}
-                component={SplashScreen}
-                options={{ fullScreenGestureEnabled: false }}
-            />
-            <Stack.Screen
-                name={routes.MAINNAVIGATOR}
-                component={MainNavigator}
-                options={{ fullScreenGestureEnabled: false }}
-            />
-        </Stack.Navigator>
-    )
+  useEffect(() => {
+    const apply = (state) => {
+      const isActive = state === "active";
+      IdleTimerManager.setIdleTimerDisabled(Boolean(authenticated) && isActive);
+    };
+
+    apply(AppState.currentState);
+
+    const sub = AppState.addEventListener("change", apply);
+    return () => {
+      sub.remove();
+      IdleTimerManager.setIdleTimerDisabled(false);
+    };
+  }, [authenticated]);
+
+  return (
+    <Stack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName={routes.SPLASH}
+    >
+      <Stack.Screen
+        name={routes.SPLASH}
+        component={SplashScreen}
+        options={{ fullScreenGestureEnabled: false }}
+      />
+      <Stack.Screen
+        name={routes.MAINNAVIGATOR}
+        component={MainNavigator}
+        options={{ fullScreenGestureEnabled: false }}
+      />
+    </Stack.Navigator>
+  );
 };
 
 export default BaseNavigator;
