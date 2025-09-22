3db66f4722257449b8307a84811f94499ef93d7e
mohammad reza <mojaver77@gmail.com>
2025-09-22 17:03:33 +0330

    feat: delete background

--- index.js
+++ index.js
@@ -6,9 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import { setupBackgroundFetchOnce } from './src/background/setupBackgroundFetch';
-
-// start background fetch once at boot
-setupBackgroundFetchOnce();
 
 AppRegistry.registerComponent(appName, () => App);
