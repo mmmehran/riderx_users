9ab86def04e41abac74e905a24bc5e6ff88a200d
mohammad reza <mojaver77@gmail.com>
2025-09-22 22:50:15 +0330

    fix: fix navigate to choose vehicle via length

--- src/navigation/BaseNavigator.js
+++ src/navigation/BaseNavigator.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { AppState } from "react-native";
 import IdleTimerManager from "react-native-idle-timer";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
 import { useNavigation } from "@react-navigation/native";
 
 import SplashScreen from '../screens/Splash/Splash';
@@ -11,20 +11,40 @@ import useDidMountEffect from '../utils/customHooks/UseDidMountEffect';
 import {  authenticated} from '../redux/reducers/authenticationReducer';
 import {setConfig,setConfigTest} from '../services/defaultAxios'
 import routes from "./routes";
+import {setSelectVehicle} from '../redux/reducers/configReducer';
+import { getData} from '../services/common.service';
+import urls from '../services/urls.json';
+import errorHandler from '../utils/errorHandler';
 
 const Stack = createNativeStackNavigator();
 
 const BaseNavigator = () => {
   const user = useSelector(authenticated);
   const navigation = useNavigation();
+  const dispatch = useDispatch();
 
+    const getVehicle = async () => {
+    const response = await getData(`${urls.GETVEHICLE}?page=1`);
+    if (response?.data?.status) {
+      if (response?.data?.data?.items?.length == 1) {
+       await dispatch(setSelectVehicle(response?.data?.data?.items[0]));
+      }
+    } else {
+      errorHandler(response);
+    }
+  };
 
-
+  const fetchRoute = async()=>{
+    if(user?.authenticated == true){
+     await  getVehicle()
+      navigation.navigate(routes.DRAWERNAVIGATOR)
+    }else{
+       navigation.navigate(routes.AUTHNAVIGATOR);
+    }
+  }
 
   useDidMountEffect(() => {
-    user?.authenticated
-      ? navigation.navigate(routes.DRAWERNAVIGATOR)
-      : navigation.navigate(routes.AUTHNAVIGATOR);
+    fetchRoute()
   }, [user?.authenticated]);
 
   useEffect(() => {
