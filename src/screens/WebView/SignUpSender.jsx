b1a66b45d2f1979c44439b7699db3ffe78dc5331
mohammad reza <mojaver77@gmail.com>
2025-12-20 15:47:22 +0330

    feat: add google login web view

--- src/screens/WebView/SignUpSender.jsx
+++ src/screens/WebView/SignUpSender.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect} from 'react';
+import React, { useCallback, useEffect } from 'react';
 import {
   View,
   SafeAreaView,
@@ -6,16 +6,26 @@ import {
   Platform,
   StatusBar,
 } from 'react-native';
-import {WebView} from 'react-native-webview';
-import {useDispatch, useSelector} from 'react-redux';
+import { WebView } from 'react-native-webview';
+import { useDispatch, useSelector } from 'react-redux';
 
 import colors from '../../config/colors';
 import {
   logout,
   authenticated,
+  login
 } from '../../redux/reducers/authenticationReducer';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { IOS_CLIENT_ID, WEB_CLIENT_ID } from "@env";
+import { postData } from '../../services/common.service';
+import urls from '../../services/urls.json';
+import errorHandler from '../../utils/errorHandler';
+import { showToast, showError } from '../../utils/helpers';
+import { setConfig } from '../../services/defaultAxios';
 
-export default function SignUpSender({route}) {
+
+
+export default function SignUpSender({ route }) {
   const dispatch = useDispatch();
   const user = useSelector(authenticated);
 
@@ -25,17 +35,63 @@ export default function SignUpSender({route}) {
       if (data?.type === 'LOGOUT') {
         dispatch(logout());
       }
-    } catch {}
+      if (data?.type === 'google_auth') {
+        handleGoogleLogin()
+      }
+    } catch { }
   }, []);
+
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      iosClientId: IOS_CLIENT_ID,
+      webClientId: WEB_CLIENT_ID,
+    });
+  }, []);
+
+  const handleGoogleLogin = async () => {
+    try {
+      if (Platform.OS === 'android') {
+        await GoogleSignin.hasPlayServices({
+          showPlayServicesUpdateDialog: true,
+        });
+      }
+      const userInfo = await GoogleSignin.signIn();
+      const token = await GoogleSignin.getTokens();
+
+      setConfig();
+      await new Promise(r => setTimeout(r, 300));
+
+      const response = await postData(
+        urls.SOCIALLOGIN,
+        {
+          access_token: token?.accessToken,
+        },
+        false,
+      );
+
+      if (response?.data?.status) {
+        if (response?.data?.data) {
+          dispatch(login(response?.data?.data));
+        }
+        showToast(response?.data?.message);
+      } else {
+        errorHandler(response);
+      }
+    } catch (error) {
+      showError(error?.message || 'Something went wrong');
+    } finally {
+    }
+  };
 
   return (
     <View style={styles.container}>
       <SafeAreaView style={styles.screen}>
-        <View style={{flex: 1}}>
+        <View style={{ flex: 1 }}>
           <WebView
             key="only-web"
-            style={{flex: 1}}
-            source={{uri: 'https://s.riderx.me/register'}}
+            style={{ flex: 1 }}
+            source={{ uri: 'https://s.riderx.me/register' }}
             javaScriptEnabled
             onMessage={onMessage}
             domStorageEnabled
